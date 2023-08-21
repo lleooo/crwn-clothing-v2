f03c72b996ab3a8a5d2d88f2df78107a7dfcef43
leo_liu <leo_liu@raytrex.com>
2023-08-21 11:15:33 +0800

    add signin

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import {initializeApp} from "firebase/app";
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAAF7_j-0dHzTDwYdQhS0cvQ1b2iAi-BVg",
@@ -20,3 +26,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'user', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error);
+        }
+    }
+
+    return userDocRef;
+};
